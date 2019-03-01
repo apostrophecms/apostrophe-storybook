183db9239505f91d5f3c1f5be80e5dfe7915d2bb
Gregory Dunn <grdunn@gmail.com>
2019-03-01 13:55:35 -0500

    add theme support to component

--- src/components/AppTitleOne.vue
+++ src/components/AppTitleOne.vue
@@ -1,20 +1,26 @@
 <template>
-  <h1 class="apos-title-one">{{ text }}</h1>
+  <h1 :class="`apos-theme--${theme} apos-title-one`">{{ text }}</h1>
 </template>
 
 <script>
 module.exports = {
   props: {
-    text: String
+    text: String,
+    theme: String
   }
 }
 </script>
 
 <style lang="scss">
 .apos-title-one {
-  color: $gray;
+  color: #f1f1f1;
   font-family: $font-family;
   font-weight: 700;
-
+  &.apos-theme--light {
+    color: $gray;
+  }
+  &.apos-theme--dark {
+    color: #000;
+  }
 }
 </style>
