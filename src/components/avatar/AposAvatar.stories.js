a0c38e9aa34f01e5f87eafc630be6cecd46070be
Alex Bea <alex.bea@gmail.com>
2019-06-28 16:57:44 -0500

    Adds avatar component

---
+++ src/components/avatar/AposAvatar.stories.js
@@ -0,0 +1,13 @@
+import { storiesOf } from '@storybook/vue'
+
+import AposAvatar from './AposAvatar.vue'
+
+storiesOf('Avatars', module)
+  .add('Avatar (single src file)', () => ({
+    components: { AposAvatar },
+    template: '<AposAvatar src="/avatars/gabrielle.jpg" alt="Gabrielle Giffords"/>'
+  }))
+  .add('Avatar (single src file, large)', () => ({
+    components: { AposAvatar },
+    template: '<AposAvatar src="/avatars/toni.jpg" size="120px" alt="Toni Morrison"/>'
+  }))
