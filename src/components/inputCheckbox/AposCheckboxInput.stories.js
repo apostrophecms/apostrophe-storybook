2c12985b2a3a4f0090fc8fa062a732bf6a14af09
Alex Bea <alex.bea@gmail.com>
2019-07-22 10:58:54 -0500

    Sets up styling for checkboxes

---
+++ src/components/inputCheckbox/AposCheckboxInput.stories.js
@@ -0,0 +1,61 @@
+import { storiesOf } from '@storybook/vue'
+
+import AposCheckboxInput from './AposCheckboxInput.vue'
+
+const field = {
+  mandatory: true,
+  name: 'turtleLeads',
+  type: 'radio',
+  label: 'Which toppings?',
+  choices: [
+    {
+      label: 'Banana peppers',
+      value: 'Banana peppers'
+    },
+    {
+      label: 'Mushrooms',
+      value: 'Mushrooms',
+      indeterminate: true
+    },
+    {
+      label: 'Cold cheese',
+      value: 'Cold cheese'
+    },
+    {
+      label: 'Anchovies',
+      value: 'Anchovies'
+    }
+  ]
+}
+
+const baseTemplate = `<AposCheckboxInput :field="field" :value="value" :status="status"/>`
+
+storiesOf('Inputs (Checkbox)', module)
+  .add('Checkbox', () => ({
+    components: { AposCheckboxInput },
+    data () {
+      return {
+        field,
+        status: {},
+        value: {
+          data: null
+        }
+      }
+    },
+    template: baseTemplate
+  }))
+  .add('Checkbox (disabled)', () => ({
+    components: { AposCheckboxInput },
+    data () {
+      return {
+        field,
+        status: {
+          disabled: true
+        },
+        value: {
+          data: null
+        }
+      }
+    },
+    template: baseTemplate
+  }))
---
+++ src/components/inputCheckbox/AposCheckboxInput.vue
@@ -0,0 +1,73 @@
+<template>
+  <AposInputWrapper :field="field" :error="status.error" >
+    <template slot="body">
+      <label class="apos-choice-label" :for="choice.value"
+        v-for="choice in field.choices" :key="choice.value">
+        <input type="checkbox" class="apos-input--choice apos-input--checkbox"
+          :value="choice.value" :name="field.name" :id="choice.value"
+          @click="toggle(choice)" :disabled="status.disabled"/>
+        <span class="apos-input-indicator" aria-hidden="true">
+          <component :is="`${
+              choice.indeterminate ? 'MinusIcon' : 'CheckBoldIcon'
+            }`"
+            :size="10" v-if="checked.includes(choice.value)"></component>
+        </span>
+        {{ choice.label }}
+      </label>
+    </template>
+  </AposInputWrapper>
+</template>
+
+<script>
+import AposInputWrapper from '../AposInputWrapper';
+import AposInputMixin from '../../mixins/AposInputMixin';
+import CheckBoldIcon from "vue-material-design-icons/CheckBold.vue";
+import MinusIcon from "vue-material-design-icons/Minus.vue";
+
+export default {
+  name: 'AposCheckboxInput',
+  mixins: [ AposInputMixin ],
+  data () {
+    return {
+      checked: []
+    }
+  },
+  components: {
+    AposInputWrapper,
+    CheckBoldIcon,
+    MinusIcon
+  },
+  methods: {
+    toggle: function (choice) {
+      if (this.checked.includes(choice.value)) {
+        this.checked = this.checked.filter(value => {
+          return value !== choice.value;
+        })
+      } else {
+        this.checked.push(choice.value);
+      }
+    },
+    validate(value) {
+      if (this.field.required) {
+        if (!value.length) {
+          return 'required';
+        }
+      }
+      if (this.field.min) {
+        if (value.length && (value.length < this.field.min)) {
+          return 'min';
+        }
+      }
+      if (this.field.max) {
+        if (value.length && (value.length > this.field.max)) {
+          return 'max';
+        }
+      }
+      return false;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
