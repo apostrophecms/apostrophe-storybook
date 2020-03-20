e48aacea42e368430f3377bd5ff31c3e163a8b8c
Stuart Romanek <stuart@apostrophecms.com>
2020-03-19 23:11:52 -0400

    adds storysource addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,19 @@
+const path = require('path');
 module.exports = {
-  stories: ['../src/components/**/*.stories.js']
+  stories: ['../src/components/**/*.stories.js'],
+  addons: [
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          // test: [/\.stories\.jsx?$/], This is default
+          include: [path.resolve(__dirname, '../src')], // You can specify directories
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false },
+        },
+      },
+    },
+    '@storybook/addon-a11y/register'
+  ],
 }
