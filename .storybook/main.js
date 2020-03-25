9827208987d105f9371db8d486d4569e635f2c82
= <hello@ghbot.com>
2020-03-25 14:30:57 -0400

    more merge

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
   stories: ['../src/components/**/*.stories.js'],
   addons: [
     '@storybook/addon-knobs',
+    '@storybook/addon-a11y/register',
     '@storybook/addon-contexts/register',
     {
       name: '@storybook/addon-storysource',
