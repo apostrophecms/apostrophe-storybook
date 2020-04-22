e5023be644ce7f3128c2a48c6b0812d53c4058ac
Alex Bea <abea@mailbox.org>
2020-04-22 10:35:21 -0500

    Fixes the avatar path for the static site

--- src/components/avatar/AposAvatar.stories.js
+++ src/components/avatar/AposAvatar.stories.js
@@ -5,9 +5,9 @@ import AposAvatar from './AposAvatar.vue';
 storiesOf('Avatars', module)
   .add('Avatar (single src file)', () => ({
     components: { AposAvatar },
-    template: '<AposAvatar src="/avatars/gabrielle.jpg" alt="Gabrielle Giffords"/>'
+    template: '<AposAvatar src="avatars/gabrielle.jpg" alt="Gabrielle Giffords"/>'
   }))
   .add('Avatar (single src file, large)', () => ({
     components: { AposAvatar },
-    template: '<AposAvatar src="/avatars/toni.jpg" size="120px" alt="Toni Morrison"/>'
+    template: '<AposAvatar src="avatars/toni.jpg" size="120px" alt="Toni Morrison"/>'
   }));
